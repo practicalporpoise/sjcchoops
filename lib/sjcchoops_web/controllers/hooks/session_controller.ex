900ecff4079441309fb820996d87991d9879550a
David Corwin <dshvazz@gmail.com>
2020-02-14 15:24:13 -0800

    Use current date

--- lib/sjcchoops_web/controllers/hooks/session_controller.ex
+++ lib/sjcchoops_web/controllers/hooks/session_controller.ex
@@ -1,7 +1,9 @@
 defmodule SJCCHoopsWeb.Hooks.SessionController do
   use SJCCHoopsWeb, :controller
 
-  def create(conn, %{"session" => %{"date" => date_param}}) do
+  @timezone "America/Los_Angeles"
+
+  def create(conn, %{"date" => date_param}) do
     case Date.from_iso8601(date_param) do
       {:ok, date} ->
         SJCCHoops.send_daily_emails(date)
@@ -15,5 +17,14 @@ defmodule SJCCHoopsWeb.Hooks.SessionController do
         |> put_status(:bad_request)
         |> json(%{status: :bad_request, message: msg})
     end
+  end
+
+  def create(conn, _param) do
+    create(conn, %{"date" => today()})
+  end
+
+  defp today do
+    {:ok, right_now} = DateTime.now(@timezone)
+    Date.to_iso8601(DateTime.to_date(right_now))
   end
 end
